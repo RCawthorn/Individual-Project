d7811cad186e85d4b86fa35bf0c6adb235097d37
Ryan <R.Cawthorn@hotmail.co.uk>
2019-11-04 10:10:42 +0000

    maybe a fix

--- School/src/main/java/com/nationwide/demo/school/Application.java
+++ School/src/main/java/com/nationwide/demo/school/Application.java
@@ -1,4 +1,4 @@
-package com.nationwide.school;
+package com.nationwide.demo.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ School/src/main/java/com/nationwide/school/controller/RestController.java
@@ -0,0 +1,4 @@
+package com.nationwide.school.controller;
+
+public @interface RestController {
+}
