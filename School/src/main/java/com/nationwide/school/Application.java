199ecf3eddcd70f84366d187c5ad993be482deec
Ryan <R.Cawthorn@hotmail.co.uk>
2019-11-04 10:25:56 +0000

    please work

--- School/src/main/java/com/nationwide/school/Application.java
+++ School/src/main/java/com/nationwide/school/Application.java
@@ -1,4 +1,4 @@
-package com.nationwide.demo.school;
+package com.nationwide.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
